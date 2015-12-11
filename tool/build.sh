ddcce3cba16fab486a5a071178699856a2ef9120
Kenneth Endfinger <kaendfinger@gmail.com>
2015-12-11 09:46:19 -0500

    Update build.sh

--- tool/build.sh
+++ tool/build.sh
@@ -10,7 +10,7 @@ mkdir build
 
 pub get
 cp -R -L packages/ build/
-cp -R bin lib build/
+cp -R bin lib dslink.json build/
 dart tool/package_map.dart
 cd build
 zip -r ../../../files/dslink-dart-digital-ocean.zip .
